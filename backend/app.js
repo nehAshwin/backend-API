d901634ce76df958ce8362f2ace521268824d60b
nehAshwin <nashwin2548@gmail.com>
2024-08-26 16:51:07 -0700

    getAllBlogs get request completed

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/user-routes.js';
+import blogRouter from './routes/blog-routes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/user',router);
+app.use('/api/blog', blogRouter);
 
 // app.use("/api", (req,res,next)=>{
 //     res.send("fuck this");
