11b41f66a071453a50ce7c5d4ea08b224430d126
nehAshwin <nashwin2548@gmail.com>
2024-08-26 17:17:37 -0700

    add user request created

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -13,3 +13,21 @@ export const getAllBlogs = async(req, res, next) => {
     }
     return res.status(200).json({blogs})
 }
+
+export const addBlog = async(req, res, next) => {
+    // created request.body field to recieve from frontend
+    const {title, description, image, user} = req.body;
+
+    const blog = new Blog({
+        title, 
+        description, 
+        image, 
+        user
+    });
+    try {
+        await blog.save();
+    } catch (err) {
+        return console.log(err);
+    }
+    return res.status(200).json({blog});
+}
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllBlogs } from "../controllers/blog-controller.js";
+import { getAllBlogs, addBlog } from "../controllers/blog-controller.js";
 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
+blogRouter.post("/add", addBlog);
 
 export default blogRouter;
